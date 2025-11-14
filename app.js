dc64bcdf6dee84e65a002cfacceba3168c11ea58
mengly-uch-cadt <mengly.uch@student.cadt.edu.kh>
2025-11-14 20:09:40 +0700

    fix: revert message in root route to original greeting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, GitHub Actions! Update for CI/CD deployment step.');
+  res.send('Hello, GitHub Actions!');
 });
 
 module.exports = app;
