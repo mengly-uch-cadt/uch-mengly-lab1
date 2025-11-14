70bb518eba7a32ab1c873dc085d1bcd4ec66956e
mengly-uch-cadt <mengly.uch@student.cadt.edu.kh>
2025-11-14 19:56:50 +0700

    feat: add deployment step to CI workflow for Render

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, GitHub Actions!');
+  res.send('Hello, GitHub Actions! Update for CI/CD deployment step.');
 });
 
 module.exports = app;
